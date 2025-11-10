65c4cc00dc4a3e0611aaa4a9b4bd727784e99bdf
RafaelRian22 <rafaelriantavares123@gmail.com>
2025-11-10 19:30:27 +0000

    novo commit

--- xadrez.c
+++ xadrez.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(){
-  int torre=1,bispo=1;
+  int torre=1,bispo=1, movimentocavalo=1;
   
   while(torre <= 5){
      printf("torre: direita.\n");
@@ -20,6 +20,17 @@ int main(){
   for(int rainha = 1; rainha <= 8; rainha++){
     printf("rainha: esquerda.\n");
   }
+  printf("\n");
+  while (movimentocavalo <= 1)
+  {
+    movimentocavalo++;
+    printf("cavalo:{\n");
+    for(int movimentocavalo2=1; movimentocavalo2 <=2; movimentocavalo2++ ){
+      printf("baixo\n");
+    }
+    printf("esquerda\n}");
+  }
 return 0;
 }
+
 
